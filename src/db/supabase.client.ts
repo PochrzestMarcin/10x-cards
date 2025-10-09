211a0281a0e1c0968b2f929031ca05f6bcc88fe4
Marcin Pochrzest <marcin.pochrzest@wavestone.eu>
2025-10-09 11:36:20 +0200

    fixes in .env

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-
 import type { Database } from './database.types';
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+export const DEFAULT_USER_ID = "cdd0d334-cfc8-45ff-836e-616a7ded0e75";
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export type SupabaseClient = typeof supabaseClient;
